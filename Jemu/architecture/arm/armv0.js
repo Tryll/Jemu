a3933635051a67eaab9d61c3dac53677cc4976a3
Amund Nettelhorst Letrud <amund@tryll.com>
2023-03-16 00:12:13 +0100

    .

--- Jemu/architecture/arm/armv0.js
+++ Jemu/architecture/arm/armv0.js
@@ -1,12 +1,24 @@
-
-//https://developer.arm.com/documentation/ddi0406/c/Application-Level-Architecture/ARM-Instruction-Set-Encoding/ARM-instruction-set-encoding?lang=en
-//https://android.googlesource.com/platform/external/lldb/+/bf5a66b/source/Plugins/Instruction/ARM/EmulateInstructionARM.cpp
-//https://opensource.apple.com/source/lldb/lldb-167.2/source/Plugins/Instruction/ARM/EmulateInstructionARM.cpp.auto.html
-// GetARMOpcodeForInstruction 
-
-
-
-// https://web.eecs.umich.edu/~prabal/teaching/eecs373-f10/readings/ARMv7-M_ARM.pdf
+/*
+ * Copyright (c) 2023 Tryll AS
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 var armPipelineWidth=8;
 var thumbPipelineWidth=4;
--- Jemu/gdb/GdbServer.js
+++ Jemu/gdb/GdbServer.js
@@ -1,15 +1,30 @@
+/*
+ * Copyright (c) 2023 Tryll AS
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 const fs = require('fs');
 const net = require('net');
 const GdbProtocolBase = require('./GdbProtocolBase.js');
 
 const TARGET_SIGTRAP=5
-
-
-//https://ftp.gnu.org/old-gnu/Manuals/gdb/html_node/gdb_129.html
-// 01 is thread id, TARGET_SIGINT = 2, TARGET_SIGTRAP = 5 
-// https://android.googlesource.com/platform/external/qemu/+/3026693afde60618212d0c1db03466535af9d2be/gdbstub.c
-
-
 
 class GdbServer {
 
